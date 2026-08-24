b74bdf8b737b053401c8825e8c3c68749f738143
Burak Yakan <burakyakan35@gmail.com>
2026-08-24 17:48:21 +0300

    searh bar centered

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,14 +5,23 @@ function Header() {
 
   return (
     <header>
-      <h1><a href="#" className={style.logoText}>birazıburada</a></h1>
-      <nav>
-        <ul>
-          <input className={style.searchBar} type="search" placeholder='Ürün, Kategori veya Marka Ara' />
-          <a href="#">Sepet</a>
-          <a href="#">Hesabım</a>
-        </ul>
-      </nav>
+      <div className={style.logoWrapper}>
+        <h1><a href="#" className={style.logoText}>birazıburada</a></h1>
+      </div>
+
+      <div className={style.searchBarWrapper}>
+        <input className={style.searchBar} type="search" placeholder='Ürün, Kategori veya Marka Ara' />
+      </div>
+
+      <div className={style.menuWrapper}>
+        <nav>
+          <ul>
+            <a href="#">Sepet</a>
+            <a href="#">Hesabım</a>
+          </ul>
+        </nav>
+      </div>
+
     </header>
   );
 
