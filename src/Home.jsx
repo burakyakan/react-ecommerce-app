2565903d18073963cfa033cb92d9cd0077d03372
Burak Yakan <burakyakan35@gmail.com>
2026-08-25 17:38:28 +0300

    added favicon v1

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,11 @@ import productsData from './products.json'
 import style from './Home.module.css'
 import Filter from './Filter';
 
+//TODO: Link ile artık öbür sitelere gitme gelmeli,
+//TODO: Product Detailed Sayfasını yap artık.
+//TODO: Sepete Ekleme ve Likelama fonksiyonu.
+//TODO: Genel search bar çalışmalı.
+
 
 function Home() {
 
